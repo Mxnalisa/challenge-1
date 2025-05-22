7c43819607271dc7d0e7f39651166d591d1e103a
Mxnalisa <amomalungs@gmail.com>
2025-05-22 08:04:58 +0200

    Updated the search(event) function

--- script.js
+++ script.js
@@ -1,10 +1,34 @@
 function search(event) {
   event.preventDefault();
-  let searchInputElement = document.querySelector("#search-input");
-  let cityElement = document.querySelector("#current-city");
-  cityElement.innerHTML = searchInputElement.value;
-}
 
+  let searchInputElement = document.querySelector("#search-input");
+  let city = searchInputElement.value;
+
+  let cityElement = document.querySelector("#current-city");
+  cityElement.innerHTML = city;
+
+  let apiKey = "d7852e3faob94308902a4f5e758t6a40";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
+
+  axios.get(apiUrl).then(function (response) {
+    let temperature = Math.round(response.data.temperature.current);
+    let temperatureElement = document.querySelector(
+      ".current-temperature-value"
+    );
+
+    temperatureElement.innerHTML = `${temperature}°C`;
+
+    let humidity = response.data.temperature.humidity;
+    let windSpeed = Math.round(response.data.wind.speed);
+    let weatherDescription = response.data.condition.description;
+
+    let detailsElement = document.querySelector(".current-details");
+    detailsElement.innerHTML = `
+      ${formatDate(new Date())}, ${weatherDescription} <br />
+      Humidity: <strong>${humidity}%</strong>, Wind: <strong>${windSpeed} km/h</strong>
+    `;
+  });
+}
 function formatDate(date) {
   let minutes = date.getMinutes();
   let hours = date.getHours();
